9dc4fa93dcaafe9fecee30cab1e23d09c5707477
luiz <LuizH-rep>
2025-07-18 10:23:48 -0300

    ADD pagina artistas

---
+++ JavaScriptp/artistas.js
@@ -0,0 +1,168 @@
+const tema = localStorage.getItem("tema");
+
+if (tema === "escuro") {
+  document.body.style.backgroundColor = "rgb(15, 23, 42)";
+  document.body.style.color = "rgb(255, 251, 245)";
+
+document.getElementById("header").style.backgroundColor = "rgb(3, 169, 244)";
+
+  document.getElementById("footer").style.backgroundColor = "rgb(3, 169, 244)"
+document.getElementById("footer").style.color = "rgb(255, 251, 245)"
+document.getElementById("topo").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("topo").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard1").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt1").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt1").style.color = "rgb(255, 251, 245)"
+ 
+document.getElementById("ARcard2").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt2").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt2").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard3").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt3").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt3").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard4").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt4").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt4").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard5").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt5").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt5").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard6").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt6").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt6").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard7").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt7").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt7").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard8").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt8").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt8").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard9").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt9").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt9").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard10").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt10").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt10").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard11").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt11").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt11").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard12").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt12").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt12").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard13").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt13").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt13").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard14").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt14").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt14").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard15").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt15").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt15").style.color = "rgb(255, 251, 245)"
+
+document.getElementById("ARcard16").style.backgroundColor = "rgb(55, 71, 79)"
+document.getElementById("ARbt16").style.backgroundColor = "rgb(15, 23, 42)"
+document.getElementById("ARbt16").style.color = "rgb(255, 251, 245)"
+
+} 
+
+else {
+  document.body.style.backgroundColor = "rgb(255, 243, 224)";
+  document.body.style.color = "rgb(38, 50, 56)";
+
+document.getElementById("header").style.backgroundColor = "rgb(245, 124, 0)";
+
+  document.getElementById("footer").style.backgroundColor = "rgb(245, 124, 0)"
+document.getElementById("footer").style.color = "rgb(38, 50, 56)"
+document.getElementById("topo").style.backgroundColor = "rgb(255, 224, 178)"
+document.getElementById("topo").style.color = "rgb(38, 50, 56)"
+
+document.getElementById("ARcard1").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt1").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt1").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard2").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt2").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt2").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard3").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt3").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt3").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard4").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt4").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt4").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard5").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt5").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt5").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard6").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt6").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt6").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard7").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt7").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt7").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard8").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt8").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt8").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard9").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt9").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt9").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard10").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt10").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt10").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard11").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt11").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt11").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard12").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt12").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt12").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard13").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt13").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt13").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard14").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt14").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt14").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard15").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt15").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt15").style.color = "rgb(38, 50, 56)";
+
+document.getElementById("ARcard16").style.backgroundColor = "rgb(255, 224, 178)";
+document.getElementById("ARbt16").style.backgroundColor = "rgb(245, 124, 0)";
+document.getElementById("ARbt16").style.color = "rgb(38, 50, 56)";
+
+
+}
+
+
+const topo = document.getElementById("topo")
+
+function upage() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+topo.addEventListener("click", upage);
